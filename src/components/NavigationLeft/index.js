f87940b5d8a2de1b5f6d076a6714ec4d610008ff
moises_ayala <moises_ayala@itculiacan.edu.mx>
2019-09-29 15:38:53 -0600

    Se perdió el contenido de index.js en el último commit, este debería ser el bueno

---
+++ src/components/NavigationLeft/index.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import AppBar from '@material-ui/core/AppBar';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Divider from '@material-ui/core/Divider';
+import Drawer from '@material-ui/core/Drawer';
+import Hidden from '@material-ui/core/Hidden';
+import IconButton from '@material-ui/core/IconButton';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import MailIcon from '@material-ui/icons/Mail';
+import MenuIcon from '@material-ui/icons/Menu';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+
+//Componente para el menú lateral izquierdo
+//Por alguna razón no logro hacer que se vea lo que hago, tendré que seguir investigando...
+
+const drawer = (
+    <div>
+      <div className={classes.toolbar} />
+      <Divider />
+      <Typography>
+            SERVICIOS
+          </Typography>
+      <List>
+        {['Reparación', 'Cambio de Tamaño', 'Grabado', 'Pulido','Limpieza'].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+      <Typography>
+            PEDIDOS
+          </Typography>
+      <List>
+        {['En proceso', 'Terminados'].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+    </div>
+  );
+
