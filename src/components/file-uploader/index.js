dd93f364d7c1aed29154de72528573ea2cebe11a
Adriana Ochoa <15171173@itculiacan.com>
2019-10-08 17:26:25 -0600

    file-uploader funcional, sin style

--- src/components/file-uploader/index.js
+++ src/components/file-uploader/index.js
@@ -3,49 +3,46 @@ import Dropzone from "react-dropzone";
 import { useState } from "react";
 
 function FileUploader() {
-  var state = {
-    dropzone1: [],
-    dropzone2: []
-  };
-
   const [lista, setLista] = useState([]);
-  debugger;
-  return (
-    <div>
-      <Dropzone
-        onDrop={files => {
-          let files_with_preview = [];
-          files.map(file => {
-            file["preview"] = URL.createObjectURL(file);
-            files_with_preview.push(file);
-          });
 
-          const new_files = [...lista, ...files_with_preview];
-          setLista(new_files);
-        }}
-      >
-        {({ getRootProps, getInputProps }) => (
-          <div {...getRootProps()} className="">
-            <input {...getInputProps()} />
-            <div
-              style={{
-                height: "124px",
-                width: "254px",
-                backgroundColor: "white",
-                borderRadius: "4px",
-                border: "1px dashed #00000058"
-              }}
-            >
-              {lista.map(file => (
-                <div style={{ fontSize: "10px", padding: "2px 2px 0px 5px" }}>
-                  {file.path}
-                </div>
-              ))}
-            </div>
+  return (
+    <Dropzone
+      onDrop={files => {
+        let files_with_preview = [];
+        files.map(file => {
+          file["preview"] = URL.createObjectURL(file);
+          files_with_preview.push(file);
+        });
+
+        const new_files = [...lista, ...files_with_preview];
+        setLista(new_files);
+      }}
+    >
+      {({ getRootProps, getInputProps }) => (
+        <div
+          {...getRootProps()}
+          className=""
+          style={{ background: "red", width: "254px", height: "124px" }}
+        >
+          <input {...getInputProps()} />
+          <div
+            style={{
+              height: "124px",
+              width: "254px",
+              backgroundColor: "white",
+              borderRadius: "4px",
+              border: "1px dashed #00000058"
+            }}
+          >
+            {lista.map(file => (
+              <div style={{ fontSize: "10px", padding: "2px 2px 0px 5px" }}>
+                {file.path}
+              </div>
+            ))}
           </div>
-        )}
-      </Dropzone>
-    </div>
+        </div>
+      )}
+    </Dropzone>
   );
 }
 
