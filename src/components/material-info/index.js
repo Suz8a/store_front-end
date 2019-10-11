3f80d789e56c964ba05a8ec3b5184751798747e3
Anahi <14170991@itculiacan.edu.mx>
2019-10-11 16:23:03 -0600

    Cree el componente y pareceque todo bien

---
+++ src/components/material-info/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import TextField from '@material-ui/core/TextField';
+import Typography from '@material-ui/core/Typography';
+
+function MaterialInfo() {
+    return (
+        <div>
+            <Grid container direction = "column" justify = "center" alignItems = "flex-start">
+                <Typography variant = "h6" component = "h2"> Material Adjunto </Typography>
+                    <Grid container direction = "row" justify = "flex-start" alignItems = "flex-start">
+                        <TextField label = "Oro (gr)" type= "number" style = {{paddingRight: "30px"}}/>
+                        <TextField label = "Bronce (gr)" type= "number"/>
+                    </Grid>
+                    <Grid container direction = "row" justify = "flex-start" alignItems = "flex-start">
+                        <TextField label = "Plata (gr)" type= "number" style = {{paddingRight: "30px"}}/>
+                        <TextField label = "Acero Inoxidable (gr)" type= "number"/>
+                    </Grid>
+            </Grid >
+        </div>
+    )
+}
+
+export default MaterialInfo;
