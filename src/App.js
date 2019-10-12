ea768d9574bc4e329d4e5bb13eb9036ebb28e754
Anahi <14170991@itculiacan.edu.mx>
2019-10-11 18:05:56 -0600

    Creo componente y al parecer todo bien

--- src/App.js
+++ src/App.js
@@ -3,28 +3,18 @@ import { MuiThemeProvider, Grid } from "@material-ui/core";
 import "./App.css";
 import Nuevo from "./components/button";
 import theme from "./theme";
+import BudgetInfo from './components/budget-info';
 
 function App() {
   return (
     <MuiThemeProvider theme={theme}>
       <Grid container>
         <div>
-          <Nuevo
-            variantType="outlined"
-            nombre="Cancelar"
-            colorTheme="secondary"
-            buttonSize="120px"
-          ></Nuevo>
         </div>
         <div style={{ paddingLeft: "30px" }}>
-          <Nuevo
-            variantType="contained"
-            nombre="Guardar"
-            colorTheme="primary"
-            buttonSize="168px"
-          ></Nuevo>
         </div>
       </Grid>
+      <BudgetInfo/>
     </MuiThemeProvider>
   );
 }
