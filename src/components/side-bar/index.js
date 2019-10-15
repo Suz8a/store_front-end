168d65b45e4b1d7dc8f7ba0a6c271d6542a31f19
moises_ayala <moises_ayala@itculiacan.edu.mx>
2019-10-15 08:49:23 -0600

    Ya quedó SideBar, solo falta el ícono Store

--- src/components/side-bar/index.js
+++ src/components/side-bar/index.js
@@ -1,50 +1,33 @@
 import React from 'react';
 import Divider from '@material-ui/core/Divider';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-import MailIcon from '@material-ui/icons/Mail';
-import MenuIcon from '@material-ui/icons/Menu';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
 import Limpieza from '../../assets/floor-brush.svg';
 import cambioTamaño from '../../assets/scissors.svg';
 import Reparacion from '../../assets/heal.svg';
 import Grabado from '../../assets/spell.svg';
-import Tienda from '../../assets/store.svg';
 import Pulido from '../../assets/clean.svg';
 import EnProceso from '../../assets/tools-cross.svg';
 import Terminados from '../../assets/Check.svg';
-import { red } from '@material-ui/core/colors';
-
+import {CardEffects, Titles, Circles, StoreIcon} 
+from "./styled"
 //Componente para el menú lateral izquierdo
 //Por alguna razón no logro hacer que se vea lo que hago, tendré que seguir investigando...
 
-const useStyles = makeStyles({
-  card: {
-    top: 75,
-    left: -2,
-    width: 304,
-    height: 726,
-    background: '#FFFFFF',
-    boxShadow: '0px 16px 10px #00000033;',
-    borderRadius: 4,
-    opacity: 1,
-  },
-});
 
 function SideBar() {
-  const classes = useStyles();
   return (
-      <Card className={classes.card}>
+      <CardEffects>
+        <Circles></Circles>
         <div>
-      <Divider />
-      <Typography>
+      <Divider/>
+      <Titles>
             SERVICIOS
-          </Typography>
+          </Titles>
       <List>
         {['Reparación', 'Cambio de Tamaño', 'Grabado', 'Pulido','Limpieza'].map((text, index) => (
           <ListItem button key={text}>
@@ -59,9 +42,9 @@ function SideBar() {
           </ListItem>
         ))}
       </List>
-      <Typography>
+      <Titles>
             PEDIDOS
-          </Typography>
+          </Titles>
       <List>
         {['En proceso', 'Terminados'].map((text, index) => (
           <ListItem button key={text}>
@@ -74,9 +57,8 @@ function SideBar() {
         ))}
       </List>
       </div>
-      </Card>
+      </CardEffects>
   )
 }
-
 export default SideBar
 
