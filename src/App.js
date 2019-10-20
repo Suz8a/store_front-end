a56adbd0b7b3e157d71ed00ffa99f569642a6203
moises_ayala <moises_ayala@itculiacan.edu.mx>
2019-10-19 20:55:17 -0600

    Componente pedidos-top-bar terminado

--- src/App.js
+++ src/App.js
@@ -1,31 +1,13 @@
 import React from "react";
-import { MuiThemeProvider, Grid } from "@material-ui/core";
 import "./App.css";
-import Nuevo from "./components/button";
-import theme from "./theme";
+import {Grid } from "@material-ui/core";
+import PedidosTopBar from "./components/pedidos-top-bar";
 
 function App() {
   return (
-    <MuiThemeProvider theme={theme}>
       <Grid container>
-        <div>
-          <Nuevo
-            variantType="outlined"
-            nombre="Cancelar"
-            colorTheme="secondary"
-            buttonSize="120px"
-          ></Nuevo>
-        </div>
-        <div style={{ paddingLeft: "30px" }}>
-          <Nuevo
-            variantType="contained"
-            nombre="Guardar"
-            colorTheme="primary"
-            buttonSize="168px"
-          ></Nuevo>
-        </div>
+      <PedidosTopBar/>
       </Grid>
-    </MuiThemeProvider>
   );
 }
 
