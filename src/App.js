a749532b8ee867ae9ed7d9b5c3d05a5561017c35
Adriana Ochoa <15171173@itculiacan.com>
2019-10-21 09:36:39 -0600

    product-info

--- src/App.js
+++ src/App.js
@@ -1,63 +1,14 @@
 import React from "react";
 import { MuiThemeProvider, Grid } from "@material-ui/core";
 import "./App.css";
-<<<<<<< HEAD
 import Nuevo from "./components/button";
 import SizeInfo from "./components/size-info";
 import ExpansionPanel from "./components/expansion-panel";
 import DetailsTitle from "./components/details-title";
 import ClientDetails from "./components/client-details";
 import ServiceDetails from "./components/service-details";
-
-import theme from "./theme";
-<<<<<<< HEAD
-import TopLayout from './components/top-layout';
-=======
+import TopLayout from "./components/top-layout";
 import ProductInfo from "./components/product-info";
->>>>>>> c1d8bfa9d4bf3442c5a1bb431369c1c3912a5ec4
-
-function App() {
-  return (
-    <MuiThemeProvider theme={theme}>
-<<<<<<< HEAD
-      <DetailsTitle folio="1309131" />
-      <ClientDetails
-        client="Suzclem Adriana Ochoa Casillas"
-        tel="6673823990"
-        email="suadocca@gmail.com"
-      />
-      <ServiceDetails service="Reparación" product="Pulsera" weight="100" />
-      <ExpansionPanel cantidad="5000" />
-=======
-      <Grid container>
-        <div>
-          <Nuevo
-            variantType="outlined"
-            nombre="Cancelar"
-            colorTheme="secondary"
-            buttonSize="120px"
-          ></Nuevo>
-        </div>
-        <div style={{ paddingLeft: "30px" }}>
-          <Nuevo
-            variantType="contained"
-            nombre="Guardar"
-            colorTheme="primary"
-            buttonSize="168px"
-          ></Nuevo>
-        </div>
-      </Grid>
-<<<<<<< HEAD
-      <TopLayout/>
->>>>>>> 24ca8a59a96702ccce69835018244c2af1a3803e
-    </MuiThemeProvider>
-=======
-      <ProductInfo/>
-      </MuiThemeProvider>
-
->>>>>>> c1d8bfa9d4bf3442c5a1bb431369c1c3912a5ec4
-=======
-import ButtonAppBar from "./components/appBar";
 import theme from "./theme";
 import SideBar from "./components/side-bar/index";
 import SimpleCard from "./components/side-bar/card";
@@ -68,12 +19,9 @@ function App() {
   return (
     <StylesProvider injectFirst>
       <MuiThemeProvider theme={theme}>
-        <div style={{ marginLeft: "40%", marginTop: "15%" }}>
-          <FileUploader />
-        </div>
+        <ProductInfo />
       </MuiThemeProvider>
     </StylesProvider>
->>>>>>> 2267712de4b124721e377f3c2b4bcd779f047c18
   );
 }
 
--- src/components/top-layout/index.js
+++ src/components/top-layout/index.js
@@ -1,16 +1,3 @@
-<<<<<<< HEAD:src/components/top-layout/index.js
-
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import Grid from '@material-ui/core/Grid';
-import Exiticon from '@material-ui/icons/ExitToApp';
-import Logo from './logoblanco.png';
-=======
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
@@ -20,7 +7,7 @@ import Button from "@material-ui/core/Button";
 import IconButton from "@material-ui/core/IconButton";
 import Grid from "@material-ui/core/Grid";
 import Exiticon from "@material-ui/icons/ExitToApp";
->>>>>>> 2267712de4b124721e377f3c2b4bcd779f047c18:src/components/appBar/index.js
+import Logo from "./logoblanco.png";
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -35,21 +22,13 @@ const useStyles = makeStyles(theme => ({
 }));
 
 function TopLayout() {
-  const classes=useStyles();
+  const classes = useStyles();
   return (
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
           <Button color="inherit">
-<<<<<<< HEAD:src/components/top-layout/index.js
-            <img src={Logo} height="40"/>
-=======
-            <img
-              src="https://scontent.fhmo2-1.fna.fbcdn.net/v/t1.0-9/40269682_317748365639307_3448012741799837696_n.png?_nc_cat=107&_nc_oc=AQnEG7TI61-vtM1kwWsTyq-WvfwweHiJyOn2x7nqHQU2bgxtn6NeN6bSQpA-F-SlyBQ&_nc_ht=scontent.fhmo2-1.fna&oh=a4cf7752606f79b8dfa15fc96309cb01&oe=5DF53550"
-              width="60"
-              height="40"
-            />
->>>>>>> 2267712de4b124721e377f3c2b4bcd779f047c18:src/components/appBar/index.js
+            <img src={Logo} height="40" />
           </Button>
           <Grid container justify="flex-end">
             <Button color="inherit">
@@ -59,9 +38,8 @@ function TopLayout() {
           </Grid>
         </Toolbar>
       </AppBar>
-      
     </div>
-  )
+  );
 }
 
-export default TopLayout
+export default TopLayout;
