59361c4c5fc5809ccff83d93e9d9f37640d09639
Adriana Ochoa <15171173@itculiacan.com>
2019-10-22 11:33:03 -0600

    sylesprovider & muitheme

--- src/App.js
+++ src/App.js
@@ -4,14 +4,18 @@ import { Grid } from "@material-ui/core";
 import MaterialInfo from "./components/material-info";
 import FileUploader from "./components/file-uploader";
 import { MuiThemeProvider } from "@material-ui/core";
+import { StylesProvider } from "@material-ui/styles";
+import theme from "./theme";
 
 function App() {
   return (
-    <MuiThemeProvider>
-      <Grid container>
-        <FileUploader />
-      </Grid>
-    </MuiThemeProvider>
+    <StylesProvider injectFirst>
+      <MuiThemeProvider theme={theme}>
+        <Grid container>
+          <FileUploader />
+        </Grid>
+      </MuiThemeProvider>
+    </StylesProvider>
   );
 }
 
