354e048aa1844dab1fad6d3d0b9ce4f1de5c8b70
Anahi <14170991@itculiacan.edu.mx>
2019-11-03 00:13:45 -0700

    prueba

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import React from "react";
 import "./App.css";
 import { Grid } from "@material-ui/core";
 import PedidosTopBar from "./components/pedidos-top-bar";
+import Text from "./components/textFieldPrueba";
+import MuiThemeProvider from "@material-ui/core";
+import theme from "./theme";
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
+
 
 function App() {
   return (
-    <StylesProvider injectFirst={true}>
+    < StylesProvider injectFirst={true}>
       <MuiThemeProvider theme={theme}>
         <PedidosTopBar />
+        <Text/>
       </MuiThemeProvider>
     </StylesProvider>
   );
