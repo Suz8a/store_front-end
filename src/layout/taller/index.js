03e1e968dd23679d083ca3b1ebbc2647fa935f73
Adriana Ochoa <15171173@itculiacan.com>
2019-12-02 09:55:34 -0700

    workshop routes added

--- src/layout/taller/index.js
+++ src/layout/taller/index.js
@@ -1,8 +1,62 @@
-import React from "react";
-import SideBarWorkShop from "../../components/side-bar-workshop";
+import React, { useEffect, useState } from "react";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
+import SideBarWorkshop from "../../components/side-bar-workshop";
 import TopLayout from "../../components/top-layout";
+import Pedidos from "../../containers/pedidos";
+import {
+  createClient,
+  createPedido,
+  getAllClients,
+  getAllPedidos,
+  getClientById,
+  getPedidoByFolio,
+  getPedidoById,
+  updatePedido,
+  uploadImage
+} from "../../api";
+import { CircularProgress, LinearProgress } from "@material-ui/core";
+
+const data22 = [
+  {
+    folio: "12345",
+    cliente: "Suzclem Adriana Ochoa Casillas",
+    servicio: "Reparación",
+    estado: "Procesando"
+  }
+];
 
 function Taller() {
+  const [pedidos, setpedidos] = useState(undefined);
+  const [clientes, setclientes] = useState(undefined);
+
+  useEffect(async () => {
+    const result = await getAllPedidos();
+    setpedidos(result.data);
+  }, []);
+
+  useEffect(async () => {
+    const result = await getAllClients();
+    setclientes(result.data);
+  }, []);
+
+  if (pedidos === undefined || clientes === undefined)
+    return (
+      <div>
+        <LinearProgress color="secondary" />
+      </div>
+    );
+
+  var data = pedidos.map(pedido => {
+    var cliente = clientes.find(client => client.id === pedido.cliente_id);
+
+    return {
+      folio: pedido.folio,
+      cliente: `${cliente.nombre} ${cliente.apellido_paterno} ${cliente.apellido_materno}`,
+      servicio: pedido.servicio,
+      estado: pedido.estado
+    };
+  });
+
   return (
     <div>
       <div>
@@ -24,7 +78,7 @@ function Taller() {
             position: "relative"
           }}
         >
-          <SideBarWorkShop />
+          <SideBarWorkshop />
         </div>
         <div
           style={{
@@ -32,7 +86,28 @@ function Taller() {
             width: "calc(100vw - 304px)",
             height: "fit-content"
           }}
-        ></div>
+        >
+          <BrowserRouter>
+            <Switch>
+              <Route
+                path="/workshop/pedidos"
+                component={() => (
+                  <Pedidos
+                    data={data.filter(pedido => pedido.estado === "En taller")}
+                  />
+                )}
+              />
+              <Route
+                path="/workshop/pedidos-terminados"
+                component={() => (
+                  <Pedidos
+                    data={data.filter(pedido => pedido.estado === "Terminado")}
+                  />
+                )}
+              />
+            </Switch>
+          </BrowserRouter>
+        </div>
       </div>
     </div>
   );
